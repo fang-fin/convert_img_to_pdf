ed00c8f277f6f32247d794a4921909bde5156f4b
fang-fin <fangweiliver@gmail.com>
2025-01-15 15:57:24 +0800

    add gitignore

---
+++ convert_img_to_pdf.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import image2pdf    
+
+
+
+def convert_image_from_file(*files):
+    print(files)
+    
+    return
+
+
+
+if __name__ == "__main__" :
+    convert_image_from_file(sys.argv[1:])
---
+++ test.py
@@ -0,0 +1,7 @@
+print("usage")
+
+def functionA():
+    print("functionA is called")
+    
+if __name__ == "__main__" :
+    functionA()
