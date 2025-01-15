776808c723d1d0b84d94d580808f458ed944d744
fang-fin <fangweiliver@gmail.com>
2025-01-15 16:46:10 +0800

    add:add convert2pdf function

--- convert_img_to_pdf.py
+++ convert_img_to_pdf.py
@@ -1,15 +1,22 @@
 import sys
 import os
-import image2pdf    
+import img2pdf    
 
 
 
-def convert_image_from_file(*files):
-    print(files)
+def convert_image_from_file(files):
+    pdf_file_name =  os.path.basename(files[0])
     
+    with open("{name}.pdf".format(name=pdf_file_name),"wb") as pdf_file :
+        pdf_file.write(img2pdf.convert(files))
     return
 
 
 
 if __name__ == "__main__" :
+    if len(sys.argv)< 1:
+        print("Usage: python convert_img_to_pdf <file1> <file2> <file3> ....")
+    
     convert_image_from_file(sys.argv[1:])
+    print(len(sys.argv[1:]))
+    
